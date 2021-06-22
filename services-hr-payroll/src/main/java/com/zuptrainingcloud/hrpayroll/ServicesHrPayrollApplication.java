e6510965a31f12d3e969322b4a0c56acd8f204e4
carloslopeszup <carlos.lopes@zup.com.br>
2021-06-22 18:28:56 -0300

    Create service payroll

---
+++ services-hr-payroll/src/main/java/com/zuptrainingcloud/hrpayroll/ServicesHrPayrollApplication.java
@@ -0,0 +1,13 @@
+package com.zuptrainingcloud.hrpayroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicesHrPayrollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicesHrPayrollApplication.class, args);
+	}
+
+}
---
+++ services-hr-payroll/src/test/java/com/zuptrainingcloud/hrpayroll/ServicesHrPayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zuptrainingcloud.hrpayroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicesHrPayrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
