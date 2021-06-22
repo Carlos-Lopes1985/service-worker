78be77631f473e251e25e35eac594fc776f523e1
carloslopeszup <carlos.lopes@zup.com.br>
2021-06-22 17:00:17 -0300

    project hr woeker create

---
+++ services-hr-worker/src/main/java/com/zuptrainingcloud/ServicesHrWorkerApplication.java
@@ -0,0 +1,13 @@
+package com.zuptrainingcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicesHrWorkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicesHrWorkerApplication.class, args);
+	}
+
+}
