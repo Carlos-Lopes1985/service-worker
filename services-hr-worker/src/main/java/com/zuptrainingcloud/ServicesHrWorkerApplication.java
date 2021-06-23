9a294c09074cac03d788e13191d008a4f0e6602e
carloslopeszup <carlos.lopes@zup.com.br>
2021-06-23 01:19:22 -0300

    Configure eureka Client

--- services-hr-worker/src/main/java/com/zuptrainingcloud/ServicesHrWorkerApplication.java
+++ services-hr-worker/src/main/java/com/zuptrainingcloud/ServicesHrWorkerApplication.java
@@ -6,10 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 import com.zuptrainingcloud.entity.Worker;
 import com.zuptrainingcloud.repository.WorkerRepository;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class ServicesHrWorkerApplication implements CommandLineRunner{
 
