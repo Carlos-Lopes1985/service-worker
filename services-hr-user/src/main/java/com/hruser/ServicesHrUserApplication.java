abdb80a90de9705f1574a43b30c5bf94b303d927
carloslopeszup <carlos.lopes@zup.com.br>
2021-06-24 00:55:48 -0300

    project hr-user created

---
+++ services-hr-user/src/main/java/com/hruser/ServicesHrUserApplication.java
@@ -0,0 +1,13 @@
+package com.hruser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicesHrUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicesHrUserApplication.class, args);
+	}
+
+}
---
+++ services-hr-user/src/test/java/com/hruser/ServicesHrUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hruser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicesHrUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
