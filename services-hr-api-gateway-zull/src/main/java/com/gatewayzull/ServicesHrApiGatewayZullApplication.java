362cf8230ed1af15cdee0557a3ac6d92335253ac
carloslopeszup <carlos.lopes@zup.com.br>
2021-06-23 21:49:53 -0300

    project hr-gateway zull created

---
+++ services-hr-api-gateway-zull/src/main/java/com/gatewayzull/ServicesHrApiGatewayZullApplication.java
@@ -0,0 +1,13 @@
+package com.gatewayzull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicesHrApiGatewayZullApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicesHrApiGatewayZullApplication.class, args);
+	}
+
+}
---
+++ services-hr-api-gateway-zull/src/test/java/com/gatewayzull/ServicesHrApiGatewayZullApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gatewayzull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicesHrApiGatewayZullApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
