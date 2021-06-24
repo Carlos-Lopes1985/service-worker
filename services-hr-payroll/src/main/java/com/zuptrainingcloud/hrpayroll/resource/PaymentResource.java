ae5c193486d7ccc2ee30bfb66ef8271e695fc21a
carloslopeszup <carlos.lopes@zup.com.br>
2021-06-23 21:31:34 -0300

    Implentation Hystris for circuit braker in micro service

--- services-hr-payroll/src/main/java/com/zuptrainingcloud/hrpayroll/resource/PaymentResource.java
+++ services-hr-payroll/src/main/java/com/zuptrainingcloud/hrpayroll/resource/PaymentResource.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.zuptrainingcloud.hrpayroll.entity.Payment;
 import com.zuptrainingcloud.hrpayroll.service.PaymentService;
 
@@ -17,8 +18,16 @@ public class PaymentResource {
 	@Autowired
 	private PaymentService paymentService;
 	
+	@HystrixCommand(fallbackMethod = "getPaymentAlternative")
 	@GetMapping(value="/{workedId}/days/{days}")
 	public ResponseEntity<Payment> getPayment(@PathVariable Long workedId,@PathVariable Integer days){
 		return ResponseEntity.ok().body(paymentService.getPayment(workedId, days));
+	}
+	
+	public ResponseEntity<Payment> getPaymentAlternative( Long workedId, Integer days){
+		
+		Payment payment = new Payment("Brann",200.,10);
+		
+		return ResponseEntity.ok(payment);
 	}
 }
