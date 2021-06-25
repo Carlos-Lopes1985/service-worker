c50c337654dde699ec6d9484620a316bffa65b77
carloslopeszup <carlos.lopes@zup.com.br>
2021-06-25 15:06:56 -0300

    containers references

--- services-hr-user/src/main/java/com/hruser/ServicesHrUserApplication.java
+++ services-hr-user/src/main/java/com/hruser/ServicesHrUserApplication.java
@@ -1,15 +1,11 @@
 package com.hruser;
 
-import java.util.Arrays;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-import com.hruser.entity.Role;
-import com.hruser.entity.User;
 import com.hruser.repositories.RoleRepository;
 import com.hruser.repositories.UserRepository;
 
@@ -17,28 +13,12 @@ import com.hruser.repositories.UserRepository;
 @SpringBootApplication
 public class ServicesHrUserApplication implements CommandLineRunner{
 	
-	@Autowired
-	private UserRepository userRepository;
-	
-	@Autowired
-	private RoleRepository roleRepository;
-	
 	public static void main(String[] args) {
 		SpringApplication.run(ServicesHrUserApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		
-//		Role r1 = new Role(null,"ROLE_OPERATOR");
-//		Role r2 = new Role(null,"ROLE_ADMIN");
-//		
-//		roleRepository.saveAll(Arrays.asList(r1,r2));
-//		
-//		User u1 = new User(null,"Carlos","ca@gmail.com","123");
-//		User u2 = new User(null,"Carlos","ca@gmail.com","123");
-//		
-//		userRepository.saveAll(Arrays.asList(u1,u2));
 		
 	}
 
--- services-hr-worker/src/main/java/com/zuptrainingcloud/ServicesHrWorkerApplication.java
+++ services-hr-worker/src/main/java/com/zuptrainingcloud/ServicesHrWorkerApplication.java
@@ -1,36 +1,20 @@
 package com.zuptrainingcloud;
 
-import java.util.Arrays;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-import com.zuptrainingcloud.entity.Worker;
-import com.zuptrainingcloud.repository.WorkerRepository;
-
 @EnableEurekaClient
 @SpringBootApplication
 public class ServicesHrWorkerApplication implements CommandLineRunner{
 
-	@Autowired
-	private WorkerRepository workerRepository;
-	
 	public static void main(String[] args) {
 		SpringApplication.run(ServicesHrWorkerApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		
-//		Worker worker = new Worker(null, "Carlos", 200.);
-//		Worker worker2 = new Worker(null, "Roberto", 500.);
-//		Worker worker3 = new Worker(null, "Paula", 400.);
-//		Worker worker4 = new Worker(null, "Carla", 300.);
-//		
-//		workerRepository.saveAll(Arrays.asList(worker, worker2,worker3,worker4));
 	}
 
 	
