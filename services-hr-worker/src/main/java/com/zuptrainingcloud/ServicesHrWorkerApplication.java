ca87d536884f08a84ca51153f7ba12e997a6a671
carloslopeszup <carlos.lopes@zup.com.br>
2021-06-25 02:19:12 -0300

    hr-worker postgree dev profile

--- services-hr-worker/src/main/java/com/zuptrainingcloud/ServicesHrWorkerApplication.java
+++ services-hr-worker/src/main/java/com/zuptrainingcloud/ServicesHrWorkerApplication.java
@@ -25,12 +25,12 @@ public class ServicesHrWorkerApplication implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		
-		Worker worker = new Worker(null, "Carlos", 200.);
-		Worker worker2 = new Worker(null, "Roberto", 500.);
-		Worker worker3 = new Worker(null, "Paula", 400.);
-		Worker worker4 = new Worker(null, "Carla", 300.);
-		
-		workerRepository.saveAll(Arrays.asList(worker, worker2,worker3,worker4));
+//		Worker worker = new Worker(null, "Carlos", 200.);
+//		Worker worker2 = new Worker(null, "Roberto", 500.);
+//		Worker worker3 = new Worker(null, "Paula", 400.);
+//		Worker worker4 = new Worker(null, "Carla", 300.);
+//		
+//		workerRepository.saveAll(Arrays.asList(worker, worker2,worker3,worker4));
 	}
 
 	
