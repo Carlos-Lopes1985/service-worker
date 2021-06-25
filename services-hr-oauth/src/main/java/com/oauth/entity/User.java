629ca9e308ead38d8128e3ad5a39b922aada5539
carloslopeszup <carlos.lopes@zup.com.br>
2021-06-24 23:15:44 -0300

    login, jwttoken

--- services-hr-oauth/src/main/java/com/oauth/entity/User.java
+++ services-hr-oauth/src/main/java/com/oauth/entity/User.java
@@ -1,8 +1,14 @@
 package com.oauth.entity;
 
 import java.io.Serializable;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -13,7 +19,7 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class User implements Serializable {/**
+public class User implements UserDetails, Serializable {/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
@@ -24,7 +30,7 @@ public class User implements Serializable {/**
 	private String email;
 	private String password;
 	
-	private Set<Role> roles = new HashSet<>();
+	private Set<Role> roles = new HashSet<Role>();
 
 	public User(Long id, String name, String email, String password) {
 		super();
@@ -32,5 +38,41 @@ public class User implements Serializable {/**
 		this.name = name;
 		this.email = email;
 		this.password = password;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+
+		return roles.stream().map(x -> new SimpleGrantedAuthority(x.getRoleName())).collect(Collectors.toList());
+	}
+
+	@Override
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return email;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return true;
 	}
 }
